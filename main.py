a3c224f91943ed769100b06fb95bf845dec76cac
unknown <popular131@g.skku.edu>
2024-10-16 18:58:28 +0900

    add main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int] :
+    for i in range(len(nums)) :
+        num1 = nums[i]
+        for j in range(len(nums)) :
+            if i >= j : 
+                pass
+            else :
+                num2 = nums[j]
+                if num1 + num2 == target :
+                    return [i, j]
+
+if __name__ == "__main__" :
+    print(twoSum([3,3], 6))
